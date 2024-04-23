93a694b7964c68117de75240fd231a39ac1fe9d3
Felipe Reis <felipereisdaniel@hotmail.com>
2024-04-23 11:23:41 -0300

    adicionando método add e remove

--- src/EstruturaDados/ListasCirculares/ListaCircular.java
+++ src/EstruturaDados/ListasCirculares/ListaCircular.java
@@ -6,6 +6,12 @@ public class ListaCircular<T> {
     private No<T> cauda;
     private int tamanhoLista;
 
+    public ListaCircular(){
+        this.cauda = null;
+        this.cabeca = null;
+        this.tamanhoLista = 0;
+    }
+
     public int size(){
         return this.tamanhoLista;
     }
@@ -34,4 +40,41 @@ public class ListaCircular<T> {
     private T get(int index){
         return this.getNo(index).getConteudo();
     }
+
+    public void remove(int index){
+
+        if(index >= this.tamanhoLista)
+            throw new IndexOutOfBoundsException("O índice é maior que o tamanho da lista");
+
+        No<T> noAuxiliar = this.cauda;
+        if(index == 0){
+            this.cauda = this.cauda.getNoProximo();
+            this.cabeca.setNoProximo(this.cauda);
+        }else if(index == 1){
+            this.cauda.setNoProximo(this.cauda.getNoProximo().getNoProximo());
+
+        }else{
+            for( int i = 0; i < index - 1;i++){
+                noAuxiliar = noAuxiliar.getNoProximo();
+            }
+            noAuxiliar.setNoProximo(noAuxiliar.getNoProximo().getNoProximo())
+        }
+        tamanhoLista--;
+    }
+
+    public void add(T conteudo){
+        No<T> novoNo = new No<>(conteudo);
+        if(this.tamanhoLista == 0){
+            this.cabeca = novoNo;
+            this.cauda = this.cabeca;
+            this.cabeca.setNoProximo(cauda);
+        }else{
+            novoNo.setNoProximo(this.cauda);
+            this.cabeca.setNoProximo(novoNo);
+            this.cauda = novoNo;
+        }
+        this.tamanhoLista++;
+    }
+
+
 }
